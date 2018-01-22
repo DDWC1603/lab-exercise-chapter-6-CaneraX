219bb7dee3bee4dbca208e1e02fa2748b1a64135
CaneraX <33883965+CaneraX@users.noreply.github.com>
2018-01-23 01:14:39 +0800

    Updated

--- String-Exercise-1.cpp
+++ String-Exercise-1.cpp
@@ -1,13 +1,13 @@
-//include your name and matric number here
+//Adam Mustaqim bin Azmi A17DW2245
 //display the string below using the string function state below.
-
 #include <iostream>
-
+#include<string.h>
+using namespace std;
 int main()
 {
 	char engkau[10];
-
-	//stringcopy(a,"hello");
+    char a[10];
+	strcpy(engkau,"hello");
 	
 	cout<<engkau;
 
